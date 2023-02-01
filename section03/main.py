9195aea187be4d9a04453625df50a6e8d82c10ce
ivanielson <ivanielson2011@gmail.com>
2023-02-01 19:53:31 -0300

    Fits all methods

--- section03/main.py
+++ section03/main.py
@@ -1,51 +1,74 @@
+from typing import Optional, Any, List
 from fastapi import FastAPI, HTTPException, status, Response, Path, Query
+from fastapi import Depends
 # from fastapi.responses import JSONResponse
-from models import Course
+from models import Course, courses
+from time import sleep
 
 
-app = FastAPI()
+def fake_db():
+    try:
+        print('Abrindo conexão com banco de dados...')
+        sleep(1)
+    finally:
+        print('Fechando conexão com banco de dados...')
+        sleep(1)
 
 
-courses = {
-    1: {
-        "id": 1,
-        "title": "Programação para Leigos",
-        "classes": 112,
-        "hours": 58
-    },
-    2: {
-        "id": 2,
-        "title": "Algoritmos e Lógica de Programação",
-        "classes": 87,
-        "hours": 67
-    }
-}
+app = FastAPI(
+    title='Api de Cursos da Geek University',
+    version='0.0.1',
+    description='Uma API para colocar em prática os estudos do FastAPI',
+)
 
 
-@app.get('/courses', status_code=status.HTTP_200_OK)
-async def get_all_courses():
+@app.get(
+    '/courses',
+    description='Retorna todos os cursos cadastrados ou uma lista vazia',
+    summary='Retorna todos os Cursos',
+    status_code=status.HTTP_200_OK,
+    response_model=List[Course],
+    response_description='Cursos encontrados com sucesso.'
+)
+async def get_all_courses(db: Any = Depends(fake_db)):
     return courses
 
 
-@app.get('/courses/{id}', status_code=status.HTTP_200_OK)
+@app.get(
+    '/courses/{id}',
+    status_code=status.HTTP_200_OK,
+    description='Retorna o curso com ID passado na URL (Se existir)',
+    summary='Retorna um curso',
+    response_model=Course,
+    response_description='Curso encontrado com sucesso.'
+)
 async def get_course_by_id(id: int = Path(
         default=None,
         title='ID do curso',
         description='Deve ser entre 1 e 2',
         gt=0,
-        lt=3)):
+        lt=3), db: Any = Depends(fake_db)):
     try:
-        course = courses[id]
-        return course
+        for course in courses:
+            if course.id == id:
+                return course
     except KeyError:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="ID não encontrado")
 
 
-@app.post('/courses', status_code=status.HTTP_201_CREATED)
-async def create_course(course: Course):
-    if course.id not in courses:
-        courses[course.id] = course
+@app.post(
+    '/courses',
+    status_code=status.HTTP_201_CREATED,
+    description='Cadastra um novo curso com as informações passa no body',
+    summary='Cria um curso',
+    response_model=Course,
+    response_description='Curso cadastrado com sucesso.'
+)
+async def create_course(course: Course, db: Any = Depends(fake_db)):
+    ids = [value.id for value in courses]
+    if course.id not in ids:
+        courses.append(course)
         return course
     else:
         raise HTTPException(
@@ -53,23 +76,40 @@ async def create_course(course: Course):
             detail=f"Já existe um curso registrado com esse o ID {course.id}.")
 
 
-@app.put('/courses/{id}', status_code=status.HTTP_200_OK)
-async def update_course(course: Course, id: int):
-    if id in courses:
-        courses[id] = course
-        return course
+@app.put(
+    '/courses/{id}',
+    status_code=status.HTTP_200_OK,
+    description='Atualiza as informaçẽos do curso de acordo com o ID passado',
+    summary='Atualiza um curso',
+    response_model=Course,
+    response_description='Curso atualizado com sucesso.'
+)
+async def update_course(course: Course, id: int, db: Any = Depends(fake_db)):
+    for value in courses:
+        if id == value.id:
+            index = courses.index(value)
+            courses[index] = course
+            return course
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Não existe um curso com ID {id}.")
 
 
-@app.delete('/courses/{id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_course(id: int):
-    if id in courses:
-        del courses[id]
-        # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
+@app.delete(
+    '/courses/{id}',
+    status_code=status.HTTP_204_NO_CONTENT,
+    description='Deleta um curso cadastrado de acordo com ID passado',
+    summary='Deleta um curso',
+    response_description='Curso deletado com sucesso.'
+)
+async def delete_course(id: int, db: Any = Depends(fake_db)):
+    for value in courses:
+        if id == value.id:
+            index = courses.index(value)
+            del courses[index]
+            # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -77,13 +117,23 @@ async def delete_course(id: int):
         )
 
 
-@app.get('/sum')
-async def sum(a: int, b: int, c: int):
-    sum_number = a + b + c
+@app.get(
+    '/sum',
+    description='Faz a soma dos números passados',
+    summary='Retorna a soma dos números'
+)
+async def sum(a: int, b: int, c: Optional[int] = None):
+    sum_number = a + b
+    if c:
+        sum_number += c
     return {"Result": sum_number}
 
 
-@app.get('/divide')
+@app.get(
+    '/divide',
+    description='Faz a divisão do primeiro número passado pelo segundo',
+    summary='Retorna a divisão de dois números'
+)
 async def divide(
     num1: int = Query(default=None, gt=0),
     num2: int = Query(default=None, gt=0)
--- section03/models.py
+++ section03/models.py
@@ -8,3 +8,19 @@ class Course(BaseModel):
     title: str
     classes: int
     hours: int
+
+
+courses = [
+    Course(
+        id=1,
+        title="Programação para Leigos",
+        classes=112,
+        hours=58
+    ),
+    Course(
+        id=2,
+        title="Algoritmos e Lógica de Programação",
+        classes=87,
+        hours=67
+    )
+]

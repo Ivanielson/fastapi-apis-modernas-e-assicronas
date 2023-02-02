627018a72fa2caa13df13c16902fd2ade6829dfa
ivanielson <ivanielson2011@gmail.com>
2023-02-02 15:45:41 -0300

    feat: implements input validation

--- section03_p1/models.py
+++ section03_p1/models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Course(BaseModel):
@@ -8,6 +8,29 @@ class Course(BaseModel):
     title: str
     classes: int
     hours: int
+
+    @validator('title')
+    def validates_title(cls, value: str):
+        words = value.split(" ")
+        if len(words) < 3:
+            raise ValueError("O campo 'title' deve ter pelo menos 3 palvras")
+        return value
+
+    @validator('classes')
+    def validates_classes(cls, value: int):
+        if value <= 12:
+            raise ValueError(
+                "O campo 'classes' deve ter um valor maior que 12"
+            )
+        return value
+
+    @validator('hours')
+    def validates_hours(cls, value: int):
+        if value <= 10:
+            raise ValueError(
+                "O campo 'hours' deve ter um valor maior que 10"
+            )
+        return value
 
 
 courses = [

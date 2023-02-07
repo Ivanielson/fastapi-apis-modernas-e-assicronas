c556cd4d416e0839e9b4f5d9298b298007a0e634
ivanielson <ivanielson2011@gmail.com>
2023-02-07 14:51:06 -0300

    chore: Configure database access credentials

--- section04/core/configs.py
+++ section04/core/configs.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     Configurações gerais usadas na aplicação
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://fastapi:fastapi@localhost:5432/faculdade'
+    DB_URL: str = 'postgresql+asyncpg://postgres:fastapi@localhost:5432/faculdade'
     DBBaseModel = declarative_base()
 
     class Config:

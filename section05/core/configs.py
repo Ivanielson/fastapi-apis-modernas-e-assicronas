e0bdb4df762b9cad8f1a0752f3fc736e94d31be5
ivanielson <ivanielson2011@gmail.com>
2023-02-13 17:31:20 -0300

    fix: Fix authentication url with database

--- section05/core/configs.py
+++ section05/core/configs.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    DB_URL: str = "postgresql+asyncpg://postgre:fastapi@localhost:5432/faculdade"
+    DB_URL: str = "postgresql+asyncpg://postgres:fastapi@localhost:5432/faculdade"
 
     class Config:
         case_sensitive = True

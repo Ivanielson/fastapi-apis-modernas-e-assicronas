0727f6e8bf0df17a89b9e8177e9c236935c5bec4
ivanielson <ivanielson2011@gmail.com>
2023-02-17 16:05:19 -0300

    refactor: correct user_id field

--- section06/models/article_model.py
+++ section06/models/article_model.py
@@ -10,5 +10,5 @@ class ArticleModel(settings.DBBaseModel):
     title = Column(String(256))
     description = Column(String(256))
     url_source = Column(String(256))
-    User_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     creator = relationship("UserModel", back_populates="articles", lazy="joined")
